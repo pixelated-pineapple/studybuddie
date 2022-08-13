898360cf340398a08edf9e3dbc15c118192e6497
Sarah Rutledge <ambrelampino64@gmail.com>
2022-08-14 00:17:21 +0100

    first commit

---
+++ app/javascript/application.js
@@ -0,0 +1,11 @@
+// Inside app/javascript/application.js  
+//= require jquery
+
+import "@hotwired/turbo-rails"  
+import "./controllers"  
+import * as bootstrap from "bootstrap"  
+  
+let popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))  
+let popoverList = popoverTriggerList.map(function (popoverTriggerEl) {  
+  return new bootstrap.Popover(popoverTriggerEl)  
+})  
